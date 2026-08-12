ed17c41c01308d8a5b33ddf982e7f224ed6128bf
GGroup-Tech <programador@ggroup.mx>
2026-08-12 15:28:31 -0600

    d

--- app/(admin)/admin/finanzas/page.tsx
+++
@@ -1,7 +0,0 @@
-import { requireStaff } from "@/lib/supabase/staff";
-import { EnConstruccion } from "@/app/components/admin/EnConstruccion";
-
-export default async function AdminFinanzasPage() {
-  await requireStaff();
-  return <EnConstruccion seccion="Finanzas" />;
-}
